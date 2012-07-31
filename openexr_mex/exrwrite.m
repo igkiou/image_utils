6a2a72aae82b324c496de8cefb1095b3bce767a5
igkiou <igkiou@gmail.com>
2012-07-30 21:46:28 -0400

    hack to handle gray images

--- openexr_mex/exrwrite.m
+++ openexr_mex/exrwrite.m
@@ -1,6 +1,11 @@
 function exrwrite(img, a, varargin)
+% Properly handle single-dimensional images.
 
 [dim1 dim2 ndims] = size(img);
+if (ndims == 1),
+	img = repmat(img, [1 1 3]);
+	ndims = 3;
+end;
 imgTrans = single(zeros([dim2 dim1 ndims]));
 for iterDim = 1:ndims,
 	imgTrans(:, :, iterDim) = img(:, :, iterDim)';
