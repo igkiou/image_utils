ddc87d797c81e11b9e50b66f969f291eb8c690cf
igkiou <igkiou@gmail.com>
2012-08-16 10:30:46 -0400

    minor edit to compCube

--- hyperspectral/compensateCube.m
+++ hyperspectral/compensateCube.m
@@ -1,6 +1,7 @@
 function cubeCompensated = compensateCube(cube, compensation_function)
 
 [M N O] = size(cube);
+cubeCompensated = zeros(size(cube));
 for iter = 1:O,
 	cubeCompensated(:, :, iter) = cube(:, :, iter)...
 								/ compensation_function(iter);
