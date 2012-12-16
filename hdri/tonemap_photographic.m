2e26685965503216ebdd6161dd38ca320d4a544d
igkiou <igkiou@gmail.com>
2012-12-16 15:30:22 -0500

    small change to tonemap_hotographic

--- hdri/tonemap_photographic.m
+++ hdri/tonemap_photographic.m
@@ -15,7 +15,7 @@ if ((nargin < 4) || isempty(mask)),
 end;
 
 % TODO: Perhaps add parameter to exclude bottom/top 1% of pixels?
-
+% mask = (imIn(:,:,1) ~= 0) & (imIn(:,:,2) ~= 0) & (imIn(:,:,3) ~= 0);
 mask = repmat(mask, [1 1 size(imIn, 3)]);
 imOut = key / meanlogv(imIn(mask)) * imIn;
 imOut = imOut .* (1 + imOut / (burn * maxv(imOut)) ^ 2) ./ (1 + imOut);
