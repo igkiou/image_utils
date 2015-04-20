76a7231524e4304ed8bd4ac5457f2ce4cd71e521
igkiou <igkiou@gmail.com>
2015-04-19 21:54:05 -0400

    fixed libraw makefile

--- raw_mex/libraw.mk
+++ raw_mex/libraw.mk
@@ -1,4 +1,4 @@
-LIBRAWDIR = /home/igkiou/MATLAB/LibRaw-0.17.0-Alpha1/build_noopenmp
+LIBRAWDIR = /home/igkiou/cpp/LibRaw-0.17.0-Alpha1/build_noopenmp
 
 LIBRAWLIBS = -L$(LIBRAWDIR)/lib -lraw_r -lraw
 LIBS += $(LIBRAWLIBS)
